754dd28c0e9f1da5f7a1100b3c4a3d85349baf57
Alexey Bedrinsky <bedrinsky@mail.ru>
2024-08-28 17:36:19 +0300

    Update views.py
    request should be in a single form

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, template_name='home.html')
 
-def contacts(requests):
+def contacts(request):
     return render(request, template_name='contacts.html')
 
